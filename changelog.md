8f9d4d51aab17fc6604a95f59fb1aa3a89f98d13
spixi <marius.spix@web.de>
2018-12-02 23:04:53 +0100

    Fixed Rune Powder recipe and made runes attachable

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+Version 0.1.2
+-------------
+Fixed crafting recipe for Rune Powder
+
 Version 0.1.1
 -------------
 Fixed issue that runeblocks could not be removed
--- init.lua
+++ init.lua
@@ -21,33 +21,6 @@ limitations under the License.
 local MP = minetest.get_modpath(minetest.get_current_modname())
 local S, NS = dofile(MP.."/intllib.lua")
 
-local materials = {
-  { material = "wood",    resource = "group:wood",           name = S("Wood Burin"),    times = { [1] = 3.61, [2] = 2.28, [3] = 0.94 },  uses =  8 },
-  { material = "stone",   resource = "group:stone",          name = S("Stone Burin"),   times = { [1] = 2.69, [2] = 1.74, [3] = 0.79 },  uses = 11 },
-  { material = "steel",   resource = "default:steel_ingot",  name = S("Steel Burin"),   times = { [1] = 1.41, [2] = 0.97, [3] = 0.52 },  uses = 21 },
-  { material = "bronze",  resource = "default:bronze_ingot", name = S("Bronze Burin"),  times = { [1] = 1.14, [2] = 0.80, [3] = 0.46 },  uses = 26 },
-  { material = "mese",    resource = "default:mese_crystal", name = S("Mese Burin"),    times = { [1] = 0.43, [2] = 0.33, [3] = 0.24 },  uses = 70 },
-  { material = "diamond", resource = "default:diamond",      name = S("Diamond Burin"), times = { [1] = 0.37, [2] = 0.29, [3] = 0.21 },  uses = 82 }
-}
-
-local drops = {
-  ["default:stone"]              = { iron_powder =  9, mese_powder =  1 },
-  ["default:stone_with_coal"]    = { iron_powder =  9, mese_powder =  1 },
-  ["default:stone_with_iron"]    = { iron_powder = 80, mese_powder =  0 },
-  ["default:stone_with_copper"]  = { iron_powder = 17, mese_powder =  1 },
-  ["default:stone_with_tin"]     = { iron_powder = 17, mese_powder =  1 },
-  ["default:stone_with_gold"]    = { iron_powder = 58, mese_powder =  4 },
-  ["default:stone_with_mese"]    = { iron_powder =  0, mese_powder = 20 },
-  ["default:stone_with_diamond"] = { iron_powder = 28, mese_powder = 10 }
-}
-
-if minetest.get_modpath("moreores") then
-  table.insert( materials, { material = "silver",  resource = "moreores:silver_ingot",  name = S("Silver Burin"),  times = { [1] = 0.60, [2] = 0.45, [3] = 0.30 }, uses = 50 } )
-  table.insert( materials, { material = "mithril", resource = "moreores:mithril_ingot", name = S("Mithril Burin"), times = { [1] = 0.41, [2] = 0.32, [3] = 0.23 }, uses = 73 } )
-  drops["moreores:mineral_silver"]  = { iron_powder = 70, mese_powder =  2 }
-  drops["moreores:mineral_mithril"] = { iron_powder = 36, mese_powder =  6 }
-end
-
 local function display_powder(position, texture)
   local minpos, maxpos = vector.subtract(position, 6), vector.add(position, 6)
 
@@ -78,131 +51,195 @@ local function give_or_drop_item(item, user)
   end
 end
 
-minetest.register_on_punchnode(
-function(pos, node, puncher, pointed_thing)
-  local item = puncher:get_wielded_item()
-  if item and item:get_name():find("^ancientrunes:burin_.+$") then
-    local drop = drops[node.name]
-
-    if drop then
-      local rand_value = math.random(100)
-
-      if rand_value <= drop.mese_powder then
-        give_or_drop_item("ancientrunes:mese_powder", puncher)
-        display_powder(puncher:get_pos(), "ancientrunes_particle_mese_powder.png")
-      elseif rand_value <= drop.iron_powder then
-        give_or_drop_item("ancientrunes:iron_powder", puncher)
-        display_powder(puncher:get_pos(), "ancientrunes_particle_iron_powder.png")
-      end
-
-      item:add_wear(65535/item:get_tool_capabilities().groupcaps.cracky.uses)
-
-      if item:get_wear() == 0 then
-        minetest.sound_play("default_tool_breaks", {pos = pointed_thing})
-        puncher:set_wielded_item(nil)
-      else
-        puncher:set_wielded_item(item)
-      end
-    end
-
-    -- Place node back
-    minetest.set_node(pos, node)
-  end
-end
-)
-
-for i = 1, #materials do
-  local m          = materials[i]
-  local burin_name = "ancientrunes:burin_"..m.material
-
-  minetest.register_tool(burin_name, {
-    description = m.name,
-    inventory_image = "ancientrunes_tool_burin_"..m.material..".png",
-    tool_capabilities = {
-      groupcaps = {
-        cracky  = { times = m.times, uses = m.uses, maxlevel = 1 }
-      }
-    }
-  })
-
-  minetest.register_craft({
-    output = burin_name,
-    recipe = {
-      {m.resource, "default:flint", m.resource},
-      {"", "group:stick", ""},
-      {"", "group:stick", ""}
-    }
-  })
-end
-
-materials = nil
-
-local runes = {"algiz","ansuz","berkanan","dagaz","ehwaz","fehu","gyfu","hagalaz","ihwa","isaz","jera","kaunan","laguz","mannaz","naudiz","othala","peorth","raido","sowilo","thurisaz","tiwaz","ur","wynn","yngvi"}
-
-for i = 1, #runes do
-  local r                = runes[i]
-  local rune_name        = "ancientrunes:rune_"..r
-  local rune_description = S(r:gsub("^%l", string.upper))
-  local rune_image       = "ancientrunes_overlay_"..r..".png"
-
-  minetest.register_craftitem(rune_name, {
-    description     = S("@1 Rune", rune_description),
-    inventory_image = rune_image
-  })
-
-  local node_names = { stone_block             = "@1 Stone Block",
-  sandstone_block         = "@1 Sandstone Block",
-  desert_stone_block      = "@1 Desert Stone Block",
-  desert_sandstone_block  = "@1 Desert Sandstone Block",
-  silver_sandstone_block  = "@1 Silver Sandstone Block",
-  obsidian_block          = "@1 Obsidian Block" }
-
-  for k, rune_node_description in pairs(node_names) do
-    local node_name      = "default:"..k
-    local rune_node_name = "ancientrunes:"..k.."_"..r
-
-    minetest.register_node(rune_node_name, {
-      description = S(rune_node_description, rune_description),
-      is_ground_content = false,
-      groups = {cracky = 2},
-      tiles = {"default_"..k..".png^"..rune_image}
-    })
-
-    minetest.register_craft({
-      type   = "shapeless",
-      output = rune_node_name,
-      recipe = {node_name, rune_name}
-    })
-  end
-end
-
-runes = nil
-
-minetest.register_craftitem("ancientrunes:iron_powder", {
-  description = S("Iron Powder"),
-  inventory_image = "ancientrunes_item_iron_powder.png"
-})
-
-minetest.register_craftitem("ancientrunes:mese_powder", {
-  description = S("Mese Powder"),
-  inventory_image = "ancientrunes_item_mese_powder.png"
-})
-
-minetest.register_craftitem("ancientrunes:rune_powder", {
-  description = S("Rune Powder"),
-  inventory_image = "ancientrunes_item_rune_powder.png"
-})
-
-minetest.register_craft({
-  type   = "shapeless",
-  output = "ancientrunes:rune_powder 5",
-  recipe = {"ancientrunes:iron_powder 4", "ancientrunes:mese_powder"}
-})
-
 local function register_rune_recipe(output, r1, r2, r3)
   minetest.register_craft({
     output = "ancientrunes:rune_"..output,
     recipe = { r1, r2, r3 }})
+end
+
+local function attach_rune(itemstack, player, pointed_thing)
+    if pointed_thing.type ~= "node" then
+       return
+    end
+
+    if minetest.is_protected(pointed_thing.under, player:get_player_name()) then
+       return
+    end
+
+    local node = minetest.get_node(pointed_thing.under)
+    local node_name, is_default_block = node.name:gsub("default:(.*_block)","%1")
+
+    if is_default_block == 0 then
+       return
+    end
+
+    if     node_name == "stone_block"
+		or node_name == "sandstone_block"
+		or node_name == "desert_stone_block"
+		or node_name == "desert_sandstone_block"
+		or node_name == "silver_sandstone_block"
+		or node_name == "obsidian_block"
+    then
+        local rune = itemstack:get_name():sub(19)
+        minetest.set_node(pointed_thing.under, { name = "ancientrunes:"..node_name.."_"..rune })
+
+		if not minetest.settings:get_bool("creative_mode") then
+			itemstack:take_item()
+            return itemstack
+		end
+    end
+end
+
+local function register_burins()
+	local materials = {
+	  { material = "wood",    resource = "group:wood",           name = S("Wood Burin"),    times = { [1] = 3.61, [2] = 2.28, [3] = 0.94 },  uses =  8 },
+	  { material = "stone",   resource = "group:stone",          name = S("Stone Burin"),   times = { [1] = 2.69, [2] = 1.74, [3] = 0.79 },  uses = 11 },
+	  { material = "steel",   resource = "default:steel_ingot",  name = S("Steel Burin"),   times = { [1] = 1.41, [2] = 0.97, [3] = 0.52 },  uses = 21 },
+	  { material = "bronze",  resource = "default:bronze_ingot", name = S("Bronze Burin"),  times = { [1] = 1.14, [2] = 0.80, [3] = 0.46 },  uses = 26 },
+	  { material = "mese",    resource = "default:mese_crystal", name = S("Mese Burin"),    times = { [1] = 0.43, [2] = 0.33, [3] = 0.24 },  uses = 70 },
+	  { material = "diamond", resource = "default:diamond",      name = S("Diamond Burin"), times = { [1] = 0.37, [2] = 0.29, [3] = 0.21 },  uses = 82 }
+	}
+
+	local drops = {
+	  ["default:stone"]              = { iron_powder =  9, mese_powder =  1 },
+	  ["default:stone_with_coal"]    = { iron_powder =  9, mese_powder =  1 },
+	  ["default:stone_with_iron"]    = { iron_powder = 80, mese_powder =  0 },
+	  ["default:stone_with_copper"]  = { iron_powder = 17, mese_powder =  1 },
+	  ["default:stone_with_tin"]     = { iron_powder = 17, mese_powder =  1 },
+	  ["default:stone_with_gold"]    = { iron_powder = 58, mese_powder =  4 },
+	  ["default:stone_with_mese"]    = { iron_powder =  0, mese_powder = 20 },
+	  ["default:stone_with_diamond"] = { iron_powder = 28, mese_powder = 10 }
+	}
+
+	if minetest.get_modpath("moreores") then
+	  table.insert( materials, { material = "silver",  resource = "moreores:silver_ingot",  name = S("Silver Burin"),  times = { [1] = 0.60, [2] = 0.45, [3] = 0.30 }, uses = 50 } )
+	  table.insert( materials, { material = "mithril", resource = "moreores:mithril_ingot", name = S("Mithril Burin"), times = { [1] = 0.41, [2] = 0.32, [3] = 0.23 }, uses = 73 } )
+	  drops["moreores:mineral_silver"]  = { iron_powder = 70, mese_powder =  2 }
+	  drops["moreores:mineral_mithril"] = { iron_powder = 36, mese_powder =  6 }
+	end
+
+	minetest.register_on_punchnode(
+	function(pos, node, puncher, pointed_thing)
+	  local item = puncher:get_wielded_item()
+	  if item and item:get_name():find("^ancientrunes:burin_.+$") then
+		local drop = drops[node.name]
+
+		if drop then
+		  local rand_value = math.random(100)
+
+		  if rand_value <= drop.mese_powder then
+		    give_or_drop_item("ancientrunes:mese_powder", puncher)
+		    display_powder(puncher:get_pos(), "ancientrunes_particle_mese_powder.png")
+		  elseif rand_value <= drop.iron_powder then
+		    give_or_drop_item("ancientrunes:iron_powder", puncher)
+		    display_powder(puncher:get_pos(), "ancientrunes_particle_iron_powder.png")
+		  end
+
+		  item:add_wear(65535/item:get_tool_capabilities().groupcaps.cracky.uses)
+
+		  if item:get_wear() == 0 then
+		    minetest.sound_play("default_tool_breaks", {pos = pointed_thing})
+		    puncher:set_wielded_item(nil)
+		  else
+		    puncher:set_wielded_item(item)
+		  end
+		end
+
+		-- Place node back
+		minetest.set_node(pos, node)
+	  end
+	end
+	)
+
+	for i = 1, #materials do
+	  local m          = materials[i]
+	  local burin_name = "ancientrunes:burin_"..m.material
+
+	  minetest.register_tool(burin_name, {
+		description = m.name,
+		inventory_image = "ancientrunes_tool_burin_"..m.material..".png",
+		tool_capabilities = {
+		  groupcaps = {
+		    cracky  = { times = m.times, uses = m.uses, maxlevel = 1 }
+		  }
+		}
+	  })
+
+	  minetest.register_craft({
+		output = burin_name,
+		recipe = {
+		  {m.resource, "default:flint", m.resource},
+		  {"", "group:stick", ""},
+		  {"", "group:stick", ""}
+		}
+	  })
+	end
+end
+
+local function register_runes()
+	local runes = {"algiz","ansuz","berkanan","dagaz","ehwaz","fehu","gyfu","hagalaz","ihwa","isaz","jera","kaunan","laguz","mannaz","naudiz","othala","peorth","raido","sowilo","thurisaz","tiwaz","ur","wynn","yngvi"}
+
+	for i = 1, #runes do
+	  local r                = runes[i]
+	  local rune_name        = "ancientrunes:rune_"..r
+	  local rune_description = S(r:gsub("^%l", string.upper))
+	  local rune_image       = "ancientrunes_overlay_"..r..".png"
+
+	  minetest.register_craftitem(rune_name, {
+		description     = S("@1 Rune", rune_description),
+		inventory_image = rune_image,
+		on_use          = attach_rune
+	  })
+
+	  local node_names = { stone_block             = "@1 Stone Block",
+	  sandstone_block         = "@1 Sandstone Block",
+	  desert_stone_block      = "@1 Desert Stone Block",
+	  desert_sandstone_block  = "@1 Desert Sandstone Block",
+	  silver_sandstone_block  = "@1 Silver Sandstone Block",
+	  obsidian_block          = "@1 Obsidian Block" }
+
+	  for k, rune_node_description in pairs(node_names) do
+		local node_name      = "default:"..k
+		local rune_node_name = "ancientrunes:"..k.."_"..r
+
+		minetest.register_node(rune_node_name, {
+		  description = S(rune_node_description, rune_description),
+		  is_ground_content = false,
+		  groups = {cracky = 2},
+		  tiles = {"default_"..k..".png^"..rune_image}
+		})
+
+		minetest.register_craft({
+		  type   = "shapeless",
+		  output = rune_node_name,
+		  recipe = {node_name, rune_name}
+		})
+	  end
+	end
+end
+
+local function register_powder()
+	minetest.register_craftitem("ancientrunes:iron_powder", {
+	  description = S("Iron Powder"),
+	  inventory_image = "ancientrunes_item_iron_powder.png"
+	})
+
+	minetest.register_craftitem("ancientrunes:mese_powder", {
+	  description = S("Mese Powder"),
+	  inventory_image = "ancientrunes_item_mese_powder.png"
+	})
+
+	minetest.register_craftitem("ancientrunes:rune_powder", {
+	  description = S("Rune Powder"),
+	  inventory_image = "ancientrunes_item_rune_powder.png"
+	})
+
+	minetest.register_craft({
+	  output = "ancientrunes:rune_powder 5",
+	  recipe = { { "ancientrunes:iron_powder", "",                         "ancientrunes:iron_powder" },
+		         { ""                        , "ancientrunes:mese_powder", ""},
+		         { "ancientrunes:iron_powder", "",                         "ancientrunes:iron_powder" } }
+	})
 end
 
 local function register_rune_crafts()
@@ -330,4 +367,7 @@ local function register_rune_crafts()
     {x, o, x})
 end
 
+register_burins()
+register_runes()
+register_powder()
 register_rune_crafts()
